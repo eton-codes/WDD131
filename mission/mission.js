7e11482dd61381938ddc2e260fce50c48298a018
= <e.s.w@icloud.com>
2025-05-05 15:09:05 -0600

    changes for W2

---
+++ mission/mission.js
@@ -0,0 +1,21 @@
+let selectElem = document.querySelector('select');
+
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+
+//HOME WORK SECTION
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // change body to dark 
+        // change logo to dark logo
+        let selectElem('img')
+    } else {
+        // change body to light
+        // remove dark from body
+        // change logo to light logo
+    }
+}
